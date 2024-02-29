481b5c4be8efe0ee104e9f20a2c2ff9939bf221f
pgm-benobira <benoit.biragumaihimbazwe@student.arteveldehs.be>
2024-02-29 13:08:41 +0100

    ✨ Sort navigation filter

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,8 @@
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("input/assets");
+    eleventyConfig.addFilter("sortByOrder", (list) => {
+        return list.sort((a, b) => a.data.order - b.data.order);
+    })
     return {
         dir: {
             input: 'input',
